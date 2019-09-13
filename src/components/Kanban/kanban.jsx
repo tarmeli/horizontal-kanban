1df762c3d2e811f4e76b41deed0f54d5bf5fd57b
roope <roope.pajunen@gmail.com>
2019-09-13 13:22:09 +0300

    Add tasks fetching by Id

--- src/components/Kanban/kanban.jsx
+++ src/components/Kanban/kanban.jsx
@@ -1,17 +1,24 @@
 import { Board } from './';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TASKS_QUERY } from '../../data/constants';
+import { useQuery } from '@apollo/react-hooks';
 
 const Kanban = ({
-  tasks, handleTaskDelete, handleTaskState, rowWithNewTask,
-}) => (
-  <Board
-    tasks={tasks.sort((a, b) => b.priority - a.priority)}
+  handleTaskDelete, handleTaskState, rowWithNewTask,
+}) => {
+  const { loading, error, data } = useQuery(TASKS_QUERY);
+
+  if (loading) return <div>loading</div>;
+  if (error) return <div>error</div>;
+
+  return (<Board
+    tasks={data.tasksById.sort((a, b) => b.priority - a.priority)}
     onTaskMove={handleTaskState}
     onTaskDelete={handleTaskDelete}
     rowWithNewTask={rowWithNewTask}
-  />
-);
+  />);
+};
 
 Kanban.propTypes = {
   tasks: PropTypes.arrayOf(PropTypes.shape({
